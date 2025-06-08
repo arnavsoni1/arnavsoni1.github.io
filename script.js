6ba92929adab6ecf775ec442ccea7ad735ea721a
Arnav Soni <86656208+arnavsoni1@users.noreply.github.com>
2025-06-08 23:21:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,13 @@
-let start = 0;
-let timer = null;
-let running = false;
-let laps = document.getElementById("laps");
+let exp = document.getElementById("display");
 
-function format(ms) {
-  let m = Math.floor(ms / 60000).toString().padStart(2, '0');
-  let s = Math.floor((ms % 60000) / 1000).toString().padStart(2, '0');
-  let msms = (ms % 1000).toString().padStart(3, '0');
-  return `${m}:${s}:${msms}`;
+function append(a) {
+  exp.value += a;
 }
 
-function update() {
-  let now = Date.now() - start;
-  document.getElementById("time").textContent = format(now);
+function clear() {
+  exp.value = " ";
 }
 
-function toggle() {
-  let btn = document.querySelector("button");
-  if (!running) {
-    start = Date.now() - (parseInt(start) || 0);
-    timer = setInterval(update, 10);
-  } else {
-    clearInterval(timer);
-    start = Date.now() - (parseInt(start) || 0);
-  }
-  running = !running;
+function calculate() {
+    exp.value = eval(exp.value);
 }
-
-function reset() {
-  clearInterval(timer);
-  running = false;
-  start = 0;
-  document.getElementById("time").textContent = "00:00:000";
-  laps.innerHTML = "";
-}
-
-function lap() {
-  if (!running) return;
-  let time = document.getElementById("time").textContent;
-  let div = document.createElement("div");
-  div.className = "lap";
-  div.textContent = `${time}`;
-  laps.appendChild(div);
-}
-
-document.addEventListener("keydown", function(event) {
-  switch (event.code) {
-    case "Space":      
-      event.preventDefault(); 
-      toggle();
-      break;
-
-    case "Enter":      
-      lap();
-      break;
-
-    case "Digit0":     
-      reset();
-      break;
-  }
-});
