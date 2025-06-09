e5942e94746e0756c780aa5a1047f7f431e8d2dc
Arnav Soni <86656208+arnavsoni1@users.noreply.github.com>
2025-06-09 23:44:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const display = document.getElementById('display');
 function append(value) {
   display.value += value;
 }
- 
+
 function clearDisplay() {
   display.value = '';
 }
 
 function calculate() {
   try {
-      let expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/−/g, '-');
-      display.value = eval(expression);
-  }
-  catch {
-        display.value = 'Error';
+    let expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/−/g, '-');
+    display.value = eval(expression);
+  } catch {
+    display.value = 'Error';
   }
 }
